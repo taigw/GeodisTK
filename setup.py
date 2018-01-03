655316c872e468a35b126e54254e78a8480dafca
Guotai Wang <wguotai@gmail.com>
2018-01-03 17:07:50 +0000

    update python scripts

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from distutils.core import setup
 from distutils.extension import Extension
-
+import numpy as np
 package_name = 'distance_transform'
 module_name1 = 'geodesic_distance'
 module1 = Extension(module_name1,
-                    include_dirs = ['/Users/guotaiwang/anaconda/lib/python2.7/site-packages/numpy/core/include','../cpp/'],
-                    sources = ['../cpp/util.cpp', '../cpp/geodesic_distance_2d.cpp', '../cpp/geodesic_distance_3d.cpp', '../cpp/geodesic_distance.cpp'])
+                    include_dirs = [np.get_include(),'./cpp'],
+                    sources = ['./cpp/util.cpp', './cpp/geodesic_distance_2d.cpp', './cpp/geodesic_distance_3d.cpp', './cpp/geodesic_distance.cpp'])
 
 setup(name=package_name,
       ext_modules = [module1])
--- test2d.py
+++ test2d.py
@@ -4,7 +4,7 @@ import time
 from PIL import Image
 import matplotlib.pyplot as plt
 def test_geodesic_distance2d():
-    I = np.asarray(Image.open('../data/img2d.png').convert('L'), np.float32)
+    I = np.asarray(Image.open('data/img2d.png').convert('L'), np.float32)
     S = np.zeros_like(I, np.uint8)
     S[100][100] = 1
     t0 = time.time()
@@ -15,11 +15,15 @@ def test_geodesic_distance2d():
     dt2 = time.time() - t1
     print "runtime(s) of fast marching {0:}".format(dt1)
     print "runtime(s) of raster  scan  {0:}".format(dt2)
-    plt.subplot(1,3,1); plt.imshow(I)
+    plt.subplot(1,3,1); plt.imshow(I, cmap='gray')
+    plt.axis('off'); plt.title('input image')
+    
     plt.subplot(1,3,2); plt.imshow(D1)
+    plt.axis('off'); plt.title('fast marching')
+    
     plt.subplot(1,3,3); plt.imshow(D2)
+    plt.axis('off'); plt.title('ranster scan')
     plt.show()
-
 
 if __name__ == '__main__':
     test_geodesic_distance2d()
